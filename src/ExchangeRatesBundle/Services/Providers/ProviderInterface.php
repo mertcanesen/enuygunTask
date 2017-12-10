e490b2a313cfdc373ff4526d1af5484a181b5d38
Mert Can ESEN <mertcanesen@gmail.com>
2017-12-10 03:43:09 +0300

    ProviderInterface hazirlandi.

---
+++ src/ExchangeRatesBundle/Services/Providers/ProviderInterface.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mert Can ESEN
+ * Date: 10.12.2017
+ * Time: 01:10
+ */
+
+namespace ExchangeRatesBundle\Services\Providers;
+
+interface ProviderInterface
+{
+    /**
+     * @return void
+     */
+    public function request();
+
+    /**
+     * @param $data
+     * @return array
+     */
+    public function parseResponse($data);
+
+}
