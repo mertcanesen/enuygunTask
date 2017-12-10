83714e8c99e8e5828619700c030259fc638fec22
Mert Can ESEN <mertcanesen@gmail.com>
2017-12-10 05:27:51 +0300

    TCMBProvider eklendi

---
+++ src/ExchangeRatesBundle/Services/Providers/TCMBProvider.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * User: Mert Can ESEN
+ * Date: 10.12.2017
+ * Time: 03:18
+ */
+
+namespace ExchangeRatesBundle\Services\Providers;
+
+use GuzzleHttp\Client;
+
+class TCMBProvider implements ProviderInterface
+{
+    const ENDPOINT = 'http://www.tcmb.gov.tr/kurlar/today.xml';
+
+    /**
+     * @return array
+     */
+    public function request()
+    {
+        $client = new Client([]);
+
+        $response = $client->request('GET', self::ENDPOINT);
+
+        return $this->parseResponse($response->getBody()->getContents());
+    }
+
+    /**
+     * @param string $data
+     * @return array
+     */
+    public function parseResponse($data)
+    {
+        $responseObject = simplexml_load_string($data);
+        $currencyObjects = $responseObject->Currency;
+
+        $currencyArray = [
+            'USD' => 0,
+            'EUR' => 0,
+        ];
+
+        foreach ($currencyObjects as $currencyObject) {
+            if ($currencyObject['CurrencyCode'] == 'USD') {
+                $currencyArray['USD'] = (float)$currencyObject->ForexBuying;
+            } elseif ($currencyObject['CurrencyCode'] == 'EUR') {
+                $currencyArray['EUR'] = (float)$currencyObject->ForexBuying;
+            }
+        }
+
+        return $currencyArray;
+    }
+}
