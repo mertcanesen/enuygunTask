f2c9363f7969ce20cd586e4f2a054de7012933e6
Mert Can ESEN <mertcanesen@gmail.com>
2017-12-10 07:13:50 +0300

    Doviz bilgileri anasayfaya eklendi

--- src/ExchangeRatesBundle/Controller/DefaultController.php
+++ src/ExchangeRatesBundle/Controller/DefaultController.php
@@ -2,12 +2,20 @@
 
 namespace ExchangeRatesBundle\Controller;
 
+use ExchangeRatesBundle\Entity\Currency;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ExchangeRatesBundle:Default:index.html.twig');
+        $currencyData = $this->getDoctrine()
+                             ->getRepository(Currency::class)
+                             ->findOneBy([], ['created_at' => 'DESC']);
+        $viewParams = [
+            'currency' => $currencyData
+        ];
+
+        return $this->render('ExchangeRatesBundle:Default:index.html.twig', $viewParams);
     }
 }
