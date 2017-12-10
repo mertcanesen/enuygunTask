7ebdf0244eb32d685a61e8115dd8e479008fd0d0
Mert Can ESEN <mertcanesen@gmail.com>
2017-12-10 05:28:05 +0300

    FCCProvider eklendi

---
+++ src/ExchangeRatesBundle/Services/Providers/FCCProvider.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * User: Mert Can ESEN
+ * Date: 10.12.2017
+ * Time: 03:32
+ */
+
+namespace ExchangeRatesBundle\Services\Providers;
+
+use GuzzleHttp\Client;
+
+class FCCProvider implements ProviderInterface
+{
+    const ENDPOINT = 'https://free.currencyconverterapi.com/api/v5/convert?q=USD_TRY,EUR_TRY&compact=ultra';
+
+    /**
+     * @return array
+     */
+    public function request()
+    {
+        $client = new Client([]);
+
+        $response = $client->request('GET', self::ENDPOINT);
+
+        return $this->parseResponse($response->getBody()->getContents());
+    }
+
+    /**
+     * @param string $data
+     * @return array
+     */
+    public function parseResponse($data)
+    {
+        $responseObject = json_decode($data);
+
+        $currencyArray = [
+            'USD' => $responseObject->USD_TRY,
+            'EUR' => $responseObject->EUR_TRY,
+        ];
+
+        return $currencyArray;
+    }
+}
